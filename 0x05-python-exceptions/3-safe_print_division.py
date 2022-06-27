6ad1f39347640f2cd5dd70af83ef72fdebc89533
Lolly254 <mukabi54@gmail.com>
2022-06-27 11:08:47 +0000

    Lolly print division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    result = 0
     try:
         result = a / b
-        print('Inside result:{:f}'.format(result))
+        print('Inside result:{:.1f}'.format(result))
     except:
         result = None
         print("Inside result: {}".format(result))
