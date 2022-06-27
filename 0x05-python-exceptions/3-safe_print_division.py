36182846aefecb47c402d633c0c7481c0ec6ca70
Lolly254 <mukabi54@gmail.com>
2022-06-27 10:58:38 +0000

    Lolly print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except:
+        return None
+    finally:
+        print('Inside result:{:f}'.format(result))
+        print('{:d} / {:d} = {:f}'.fomrat(a, b, result)
